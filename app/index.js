35823b3ddc6f1c225f73335a7fc0372449fec7f2
William Klausmeyer <willyk95@gmail.com>
2017-12-14 16:24:38 -0600

    v1.0.1

---
+++ app/index.js
@@ -0,0 +1,100 @@
+import * as messaging from "messaging";
+import document from "document";
+
+var games = [];
+var games_visible = true;
+
+/**
+ * Displays a team inside the div
+ * @param {DOMElement} div  The game's div
+ * @param {DOMElement} team The team data
+ * @param {DOMElement} type The team type (away, home)
+ */
+function showTeam(div, team, type) {
+  div.getElementById(`${type}-score`).text = typeof team.score === 'undefined'
+    ? ''
+    : team.score;
+  div.getElementById(`${type}-name`).text = team.name;
+}
+
+/**
+ * Adds a game to the displayed item
+ * @param {Object} game The game information from the message
+ */
+function addGame(game) {
+  // See if the game already exists
+  var existing_game = games.filter(possible_game => possible_game.id === game.id);
+  if (existing_game.length === 0) {
+    game.div_id = games.length;
+    games.push(game);
+  } else {
+    existing_game[0].home = game.home;
+    existing_game[0].away = game.away;
+    existing_game[0].cast = game.cast;
+    existing_game[0].game = game.game;
+    game = existing_game[0];
+  }
+
+  // Get and update the div
+  var game_div = document.getElementById(`game-${game.div_id}`);
+  game_div.style.display = 'inline';
+  showTeam(game_div, game.away, 'away');
+  showTeam(game_div, game.home, 'home');
+
+  game_div.getElementById('game-top').text = game.game.top;
+  game_div.getElementById('game-bottom').text = game.game.bottom;
+
+  document.getElementById('updated').text = `Updated ${game.updated}`;
+
+  // Hide the remaining divs
+  var i = games.length;
+  var div;
+  while ((div = document.getElementById(`game-${i}`)) !== null) {
+    div.style.display = 'none';
+    i++;
+  }
+
+  showGames(true);
+
+  // Calculate the percentage
+  var length = 336;
+  var x_start = 6;
+  var done_pixels = Math.round((game.i + 1) * length / game.count);
+
+  var bar = document.getElementById('bar');
+  bar.x = x_start + done_pixels;
+  bar.width = length - done_pixels;
+}
+
+/**
+ * Show or hide the games screen
+ * @param {Boolean} state To show or hide the game list
+ */
+function showGames(state) {
+  if (state === games_visible) {
+    return;
+  }
+
+  document.getElementById('game-list').style.display = state
+    ? 'inline'
+    : 'none';
+
+  document.getElementById('loading').style.display = state
+    ? 'none'
+    : 'inline';
+
+  games_visible = state;
+}
+
+showGames(false);
+
+messaging.peerSocket.onmessage = evt => {
+  switch (evt.data.action) {
+    case 'add_game':
+      addGame(evt.data);
+      break;
+    case 'add_games':
+      evt.data.data.forEach(game => addGame(game));
+      break;
+  }
+};
---
+++ companion/index.js
@@ -0,0 +1,159 @@
+import * as messaging from "messaging";
+
+/**
+ * Returns a friendly tiemzone name from the offset returned from
+ * getTimezoneOffset
+ * @param  {Integer} offset The offset in seconds from getTimezoneOffset
+ * @return {String}         The friendly timezone name
+ */
+function getTimezone(offset) {
+  var timezones = {
+    240: 'AST',
+    300: 'EST',
+    360: 'CST',
+    420: 'MST',
+    480: 'PST',
+    540: 'AKST',
+    600: 'HAST',
+  };
+
+  if (typeof timezones[offset] !== 'undefined') {
+    return timezones[offset];
+  } else if (offset === 0) {
+    return 'UTC';
+  } else if (offset < 0) {
+    return 'UTC+' + (offset / 60);
+  } else {
+    return 'UTC-' + (offset / 60);
+  }
+}
+
+/**
+ * Returns a time string
+ * @param  {Date}   date A javascript date object
+ * @return {String}      H:MM AMPM TZ (e.g. 12:00 PM CST or 1:00 AM EST)
+ */
+function getTimeString(date) {
+  // Get the hour
+  var hour = date.getHours();
+  var ampm = 'AM';
+  if (hour === 0) {
+    hour = 12;
+  } else if (hour === 12) {
+    ampm = 'PM';
+  } else if (hour > 12) {
+    ampm = 'PM';
+    hour = hour - 12;
+  }
+
+  return hour + ':' + ('00' + date.getMinutes()).slice(-2) + ' ' + ampm + ' ' + getTimezone(date.getTimezoneOffset());
+}
+
+/**
+ * Sends a message to the watch and logs failures
+ * @param {Object} data The data to send
+ */
+function sendMessage(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    try {
+      messaging.peerSocket.send(data);
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    console.log('Message not sent');
+  }
+}
+
+/**
+ * Return a team object (name and score) from information from the API
+ * @param  {Object} team      game.teams.{TEAM} from the API
+ * @param  {Object} linescore game.linescore.teams.{TEAM} from the API
+ * @return {Object}           The team object with a name and score. The name
+ *                            will include the status (PP, EN)
+ */
+function getTeam(team, linescore) {
+  var status_string = '';
+  if (linescore.powerPlay && linescore.goaliePulled) {
+    status_string = '(PP, EN)';
+  } else if (linescore.powerPlay) {
+    status_string = '(PP)';
+  } else if (linescore.goaliePulled) {
+    status_string = '(EN)';
+  }
+
+  return {
+    name: team.team.abbreviation + status_string,
+    score: linescore.score,
+  };
+}
+
+/**
+ * Return the game object for the message
+ * @param  {Object} game The game object from the API
+ * @return {Object}      The game object for the watch with top and bottom text
+ */
+function getGameState(game) {
+  if (game.linescore.currentPeriod === 0) {
+    return {
+      top: getTimeString(new Date(game.gameDate)),
+      bottom: '',
+    };
+  } else if (game.status.abstractGameState === 'Final') {
+    return {
+      top: game.linescore.currentPeriodOrdinal,
+      bottom: game.linescore.currentPeriodTimeRemaining,
+    };
+  } else {
+    return {
+      top: game.linescore.currentPeriodOrdinal,
+      bottom: game.linescore.currentPeriodTimeRemaining,
+    };
+  }
+}
+
+/**
+ * Retrieve and send the games to the watch
+ * @param {String} date The date to pass into the URL (optional)
+ */
+function getGameStatus(date) {
+  // Check the state
+  if (messaging.peerSocket.readyState !== messaging.peerSocket.OPEN) {
+    console.log('NOT CONNECTED');
+    return;
+  }
+
+  // Get today's date (if needed)
+  if (typeof date === 'undefined') {
+    var today = new Date();
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  }
+
+  var url = `https://statsapi.web.nhl.com/api/v1/schedule?startDate=${date}&endDate=${date}&expand=schedule.teams,schedule.linescore&site=en_nhl`;
+
+  fetch(url, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => data.dates[0].games)
+    .then(games => games.map((game, i) => sendMessage({
+      action: 'add_game',
+      id: game.gamePk,
+      home: getTeam(game.teams.home, game.linescore.teams.home),
+      away: getTeam(game.teams.away, game.linescore.teams.away),
+      game: getGameState(game),
+      count: games.length,
+      updated: getTimeString(new Date()),
+      i: i,
+    })))
+    .then(() => setTimeout(getGameStatus, 30000))
+    .catch(e => {
+      console.log(e);
+    });
+}
+
+if (messaging.peerSocket.readyState !== messaging.peerSocket.OPEN) {
+  messaging.peerSocket.onopen = getGameStatus.bind(null, undefined);
+} else {
+  getGameStatus();
+}
