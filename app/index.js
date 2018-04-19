6b304822d905679f87a66ae7d11d182f18812ad9
klawil <klawil@a9.com>
2018-04-19 08:48:35 -0700

    Move to redux for the watch code

--- app/index.js
+++ app/index.js
@@ -1,147 +1,216 @@
-import * as messaging from "messaging";
-import document from "document";
+import * as messaging from 'messaging';
+import document from 'document';
+import store from './redux.js';
 
-var games = [];
-var games_visible = true;
-var date_offset = 0;
-var old_date = null;
-
-/**
- * Displays a team inside the div
- * @param {DOMElement} div  The game's div
- * @param {DOMElement} team The team data
- * @param {DOMElement} type The team type (away, home)
- */
-function showTeam(div, team, type) {
-  div.getElementById(`${type}-score`).text = typeof team.score === 'undefined'
-    ? ''
-    : team.score;
-  div.getElementById(`${type}-name`).text = team.name;
+const elements = {
+  screens: {
+    loading: document.getElementById('loading'),
+    no_games: document.getElementById('no-games'),
+    game_list: document.getElementById('game-list'),
+  },
+  buttons: {
+    previous: [
+      document.getElementById('no-games').getElementById('previous'),
+      document.getElementById('game-list').getElementById('previous'),
+    ],
+    next: [
+      document.getElementById('no-games').getElementById('next'),
+      document.getElementById('game-list').getElementById('next'),
+    ],
+  },
+  games: [],
+};
+let date_offset = 0;
+let game_index = 0;
+let game_div_init;
+while ((game_div_init = document.getElementById(`game-${game_index}`)) !== null) {
+  elements.games.push(game_div_init);
+  game_index++;
 }
 
 /**
- * Adds a game to the displayed item
- * @param {Object} game The game information from the message
+ * Change a div's property ONLY IF the new value is different
+ * @param {DOMElement} div The element to alter
+ * @param {String} prop The proeprty to change
+ * @param {Mixed} value The new value
+ * @return {void}
  */
-function addGame(game) {
-  // Check for a date change
-  if (game.date !== old_date) {
-    old_date = game.date;
-    clearGames();
-  }
-
-  // See if the game already exists
-  var existing_game = games.filter(possible_game => possible_game.id === game.id);
-  if (existing_game.length === 0) {
-    game.div_id = games.length;
-    games.push(game);
-  } else {
-    existing_game[0].home = game.home;
-    existing_game[0].away = game.away;
-    existing_game[0].cast = game.cast;
-    existing_game[0].game = game.game;
-    existing_game[0].updated = game.updated;
-    game = existing_game[0];
-  }
-
-  // Get and update the div
-  var game_div = document.getElementById(`game-${game.div_id}`);
-  game_div.style.display = 'inline';
-  showTeam(game_div, game.away, 'away');
-  showTeam(game_div, game.home, 'home');
-
-  game_div.getElementById('game-top').text = game.game.top;
-  game_div.getElementById('game-bottom').text = game.game.bottom;
-
-  // Hide the remaining divs
-  var i = games.length;
-  var div;
-  while ((div = document.getElementById(`game-${i}`)) !== null) {
-    div.style.display = 'none';
-    i++;
-  }
-
-  // Mark as updating or updated
-  var list = document.getElementById('game-list');
-  var bar = list.getElementById('bar');
-  var bar_fill = list.getElementById('bar-fill');
-  var updated = list.getElementById('updated');
-  if ((game.i + 1) < game.count) {
-    // Calculate the percentage
-    var length = 272;
-    var x_start = 38;
-    var done_pixels = Math.round((game.i + 1) * length / game.count);
-
-    bar.x = x_start + done_pixels;
-    bar.width = length - done_pixels;
-    bar.style.display = 'inline';
-    bar_fill.style.display = 'inline';
-    updated.style.display = 'none';
-  } else {
-    bar.style.display = 'none';
-    bar_fill.style.display = 'none';
-    updated.style.display = 'inline';
-    updated.text = `Updated ${game.updated}`;
-  }
-
-  // Show the games list
-  showGames(true, true);
-}
-
-/**
- * Show or hide the games screen
- * @param {Boolean} state To show or hide the game list
- * @param {Boolean} games Are there games for this date?
- */
-function showGames(state, games) {
-  if (state === games_visible && games) {
+function conditionalChangeProperty(div, prop, value) {
+  // Determine if the value is different
+  if (div[prop] === value) {
     return;
   }
 
-  // Show game-list if state is true
-  document.getElementById('game-list').style.display = state
-    ? 'inline'
-    : 'none';
-
-  // Show loading if state is false and games is true
-  document.getElementById('loading').style.display = !state && games
-    ? 'inline'
-    : 'none';
-
-  // Show no games if state is false and games is false
-  document.getElementById('no-games').style.display = !state && !games
-    ? 'inline'
-    : 'none';
-
-  games_visible = state;
+  // Set the new value
+  div[prop] = value;
 }
 
 /**
- * Set the date in the navigation bar
- * @param {String} date The date for the current games
+ * Hide or show a div ONLY IF the new state is different from the current state
+ * @param {DOMElement} div The element to hide or show
+ * @param {Boolean} state The new state for the div
+ * @return {void}
  */
-function setDateLabel(date) {
-  document.getElementById('game-list').getElementById('date').text = date;
-  document.getElementById('no-games').getElementById('date').text = date;
+function conditionalHideOrShowDiv(div, state) {
+  // Determine if we should change the state
+  if (state && div.style.display === 'inline') {
+    return;
+  } else if (!state && div.style.display === 'none') {
+    return;
+  }
+
+  div.style.display = state
+    ? 'inline'
+    : 'none';
+}
+
+/**
+ * Renders the GUI from the state
+ * @return {void}
+ */
+function render() {
+  // Get the state
+  var state = store.getState();
+  console.log(JSON.stringify(state));
+
+  // Handle screen state changes
+  Object.keys(state.screens)
+    .map((screen) => conditionalHideOrShowDiv(
+      elements.screens[screen],
+      state.screens[screen]
+    ));
+
+  // Handle game rendering
+  if (state.screens.game_list) {
+    // Loop over the games list
+    elements.games.map((game_div, index) => {
+      // Get the game info
+      var game_info = state.games[index];
+
+      // Determine if there is a game associated
+      if (typeof game_info === 'undefined') {
+        return conditionalHideOrShowDiv(game_div, false);
+      }
+
+      // Show the game div (if necessary)
+      conditionalHideOrShowDiv(game_div, true);
+
+      // Verify the team data
+      [
+        'home',
+        'away'
+      ].map((team) => {
+        conditionalChangeProperty(
+          game_div.getElementById(`${team}-score`),
+          'text',
+          typeof game_info[team].score === 'undefined'
+            ? ''
+            : game_info[team].score
+        );
+        conditionalChangeProperty(
+          game_div.getElementById(`${team}-name`),
+          'text',
+          game_info[team].name
+        );
+      });
+
+      // Verify the game data
+      conditionalChangeProperty(
+        game_div.getElementById('game-top'),
+        'text',
+        game_info.game.top
+      );
+      conditionalChangeProperty(
+        game_div.getElementById('game-bottom'),
+        'text',
+        game_info.game.bottom
+      );
+
+      return null;
+    });
+
+    // Set up the updated bar
+    var bar_div = elements.screens.game_list.getElementById('bar');
+    var bar_fill_div = elements.screens.game_list.getElementById('bar-fill');
+    conditionalHideOrShowDiv(
+      bar_div,
+      state.progress.done !== state.progress.total
+    );
+    conditionalHideOrShowDiv(
+      bar_fill_div,
+      state.progress.done !== state.progress.total
+    );
+    if (state.progress.done !== state.progress.total) {
+      var bar_length = 272;
+      var x_start = 38;
+      var done_pixels = Math.round((state.progress.done + 1) * bar_length / state.progress.total);
+
+      conditionalChangeProperty(
+        bar_div,
+        'x',
+        x_start + done_pixels
+      );
+      conditionalChangeProperty(
+        bar_div,
+        'width',
+        bar_length - done_pixels
+      );
+    }
+
+    // Set the updated at
+    var updated_div = elements.screens.game_list.getElementById('updated');
+    conditionalChangeProperty(
+      updated_div,
+      'text',
+      state.updated
+    );
+    conditionalHideOrShowDiv(
+      updated_div,
+      state.progress.done === state.progress.total
+    );
+  }
+
+  // Handle date
+  if (state.screens.game_list || state.screens.no_games) {
+    // Get the screen div
+    var screen_div = elements.screens[
+      state.screens.game_list
+        ? 'game_list'
+        : 'no_games'
+    ];
+
+    // Set the date
+    conditionalChangeProperty(
+      screen_div.getElementById('date'),
+      'text',
+      state.date
+    );
+  }
 }
 
 /**
  * This function changes the day being displayed
  * @param {Integer} delta The change to the date_offset
+ * @return {void}
  */
 function changeOffset(delta) {
   date_offset += delta;
   sendMessage({
     action: 'change_date',
-    offset: date_offset
+    offset: date_offset,
   });
-  clearGames();
-  showGames(false, true);
+  store.dispatch({
+    action: 'SHOW_LOADING'
+  });
+  store.dispatch({
+    action: 'CLEAR_GAMES'
+  });
 }
 
 /**
  * Sends a message to the watch and logs failures
  * @param {Object} data The data to send
+ * @return {void}
  */
 function sendMessage(data) {
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
@@ -155,33 +224,61 @@ function sendMessage(data) {
   }
 }
 
-/**
- * Hide all of the game divs
- */
-function clearGames() {
-  games.map(game => document.getElementById(`game-${game.div_id}`).style.display = 'none');
-  games = [];
-}
-
-showGames(false, true);
+store.subscribe(render);
 
 // Attach button listeners
-document.getElementById('no-games').getElementById('previous').onclick = changeOffset.bind(null, -1);
-document.getElementById('game-list').getElementById('previous').onclick = changeOffset.bind(null, -1);
-document.getElementById('no-games').getElementById('next').onclick = changeOffset.bind(null, 1);
-document.getElementById('game-list').getElementById('next').onclick = changeOffset.bind(null, 1);
+document.getElementById('no-games')
+  .getElementById('previous').onclick = changeOffset.bind(null, -1);
+document.getElementById('game-list')
+  .getElementById('previous').onclick = changeOffset.bind(null, -1);
+document.getElementById('no-games')
+  .getElementById('next').onclick = changeOffset.bind(null, 1);
+document.getElementById('game-list')
+  .getElementById('next').onclick = changeOffset.bind(null, 1);
 
-messaging.peerSocket.onmessage = evt => {
+// Show the loading screen
+store.dispatch({
+  action: 'SHOW_LOADING'
+});
+
+messaging.peerSocket.onmessage = (evt) => {
+  // Update the date
   if (typeof evt.data.date !== 'undefined') {
-    setDateLabel(evt.data.date);
+    store.dispatch({
+      action: 'SET_DATE',
+      value: evt.data.date
+    });
   }
 
   switch (evt.data.action) {
     case 'add_game':
-      addGame(evt.data);
+      store.dispatch({
+        ...evt.data,
+        action: 'ADD_GAME',
+      });
+      store.dispatch({
+        action: 'SET_PROGRESS_TOTAL',
+        value: evt.data.count,
+      });
+      store.dispatch({
+        action: 'SET_PROGRESS_DONE',
+        value: evt.data.i + 1,
+      });
+      store.dispatch({
+        action: 'SHOW_GAME_LIST',
+      });
+      store.dispatch({
+        action: 'SET_UPDATED',
+        value: evt.data.updated,
+      });
       break;
     case 'no_games':
-      showGames(false, false);
+      store.dispatch({
+        action: 'SHOW_NO_GAMES',
+      });
+      break;
+    default:
+      console.log(JSON.stringify(evt.data));
       break;
   }
 };
---
+++ app/redux.js
@@ -0,0 +1,149 @@
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
+const reducer = (state = {
+  screens: {
+    loading: true,
+    no_games: true,
+    game_list: true,
+  },
+  games: [],
+  date: 'loading',
+  updated: 'loading',
+  progress: {
+    done: 0,
+    total: 1,
+  },
+}, action) => {
+  console.log(JSON.stringify(action));
+  switch (action.action) {
+    // DISPLAY ITEMS
+    case 'SHOW_LOADING':
+      return {
+        ...state,
+        screens: Object
+          .keys(state.screens)
+          .reduce((previous, screen) => {
+            previous[screen] = screen === 'loading';
+            return previous;
+          }, {})
+      };
+    case 'SHOW_GAME_LIST':
+      return {
+        ...state,
+        screens: Object
+          .keys(state.screens)
+          .reduce((previous, screen) => ({
+            ...previous,
+            [screen]: screen === 'game_list'
+          }), {})
+      };
+    case 'SHOW_NO_GAMES':
+      return {
+        ...state,
+        screens: Object
+          .keys(state.screens)
+          .reduce((previous, screen) => ({
+            ...previous,
+            [screen]: screen === 'no_games'
+          }), {})
+      };
+
+    // Handle the date changing
+    case 'SET_DATE':
+      return {
+        ...state,
+        date: action.value
+      };
+
+    // Updated at values
+    case 'SET_UPDATED':
+      return {
+        ...state,
+        updated: action.value
+      };
+    case 'SET_PROGRESS_DONE':
+      return {
+        ...state,
+        progress: {
+          ...state.progress,
+          done: action.value
+        }
+      };
+    case 'SET_PROGRESS_TOTAL':
+      return {
+        ...state,
+        progress: {
+          ...state.progress,
+          total: action.value
+        }
+      };
+
+    // Update games
+    case 'ADD_GAME':
+      // Build the game object
+      var new_game = {
+        id: action.id,
+        home: action.home,
+        away: action.away,
+        game: action.game,
+      };
+      console.log(JSON.stringify(new_game));
+
+      // Determine if the game already exists
+      if (
+        state
+          .games
+          .filter((game) => game.id === action.id)
+          .length === 0
+      ) {
+        return {
+          ...state,
+          games: [
+            ...state.games,
+            new_game
+          ]
+        };
+      }
+      return {
+        ...state,
+        games: state.games.map((game) => {
+          if (game.id !== action.id) {
+            return game;
+          }
+
+          return new_game;
+        })
+      };
+    case 'CLEAR_GAMES':
+      return {
+        ...state,
+        games: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default createStore(reducer);
